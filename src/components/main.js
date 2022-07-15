0351d76cf9ec2e1c1c937f87e3e78ad47583441f
igorevanss <igorevans.pro@gmail.com>
2022-07-15 19:48:00 -0300

    Adição da sidebar como componente

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,6 @@
 import Stories from "./stories"
 import Feed from "./feed"
+import SideBar from "./sideBar"
 
 export default function Main() {
   return (
@@ -10,94 +11,7 @@ export default function Main() {
           <Feed />
           </div>
         </div>
-        <aside>
-          <div class="user-profile">
-            <img src="./images/4.jpg" alt="" />
-            <div>
-              <p class="user-profile-text">
-                <strong>lebonzon</strong>
-              </p>
-              <p>Legoboy</p>
-            </div>
-          </div>
-          <div class="suggestions-title">
-            <p>Sugestões para você</p>
-            <p>
-              <strong>Ver tudo</strong>
-            </p>
-          </div>
-          <div class="suggestions-profiles">
-            <div class="suggestions-profile">
-              <div class="user-profile">
-                <img src="./images/7.jpg" alt="" />
-                <div>
-                  <p>
-                    <strong>obras</strong>
-                  </p>
-                  <p>Segue você</p>
-                </div>
-              </div>
-              <p>Seguir</p>
-            </div>
-            <div class="suggestions-profile">
-              <div class="user-profile">
-                <img src="./images/2.jpg" alt="" />
-                <div>
-                  <p>
-                    <strong>lepadrinzin</strong>
-                  </p>
-                  <p>Segue você</p>
-                </div>
-              </div>
-              <p>Seguir</p>
-            </div>
-            <div class="suggestions-profile">
-              <div class="user-profile">
-                <img src="./images/6.jpg" alt="" />
-                <div>
-                  <p>
-                    <strong>lacucaracha</strong>
-                  </p>
-                  <p>Novo no Instagram</p>
-                </div>
-              </div>
-              <p>Seguir</p>
-            </div>
-            <div class="suggestions-profile">
-              <div class="user-profile">
-                <img src="./images/3 (1).jpg" alt="" />
-                <div>
-                  <p>
-                    <strong>medicines</strong>
-                  </p>
-                  <p>Segue você</p>
-                </div>
-              </div>
-              <p>Seguir</p>
-            </div>
-            <div class="suggestions-profile">
-              <div class="user-profile">
-                <img src="./images/8.jpg" alt="" />
-                <div>
-                  <p>
-                    <strong>breadman</strong>
-                  </p>
-                  <p>Segue você</p>
-                </div>
-              </div>
-              <p>Seguir</p>
-            </div>
-          </div>
-          <div class="about">
-            <p>
-              Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos
-              • Localizações • Contas mais relevantes • Hashtags • Idioma
-            </p>
-          </div>
-          <div class="rights">
-            <p>© 2021 INSTAGRAM DO FACEBOOK</p>
-          </div>
-        </aside>
+        <SideBar />
       </main>
   )
 }
